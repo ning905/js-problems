6bd2af6fbcc418afd3b1a0b94829f5b0b582baec
Ning Ma <ningma905@gmail.com>
2022-06-23 17:14:29 +0100

    fix

--- src/medium/fibonacci/solution.js
+++ src/medium/fibonacci/solution.js
@@ -1,15 +1,15 @@
 const fib = (num) => {
-    const result = []
-    for (i = 0; i < num; i++) {
-        if (i < 2) {
-            fibNum = 1
-            result.push(fibNum)
-        } else {
-            fibNum = result[i - 1] + result[i - 2]
-            result.push(fibNum)
-        }
+  const result = [];
+  for (i = 0; i < num; i++) {
+    if (i < 2) {
+      const fibNum = 1;
+      result.push(fibNum);
+    } else {
+      const fibNum = result[i - 1] + result[i - 2];
+      result.push(fibNum);
     }
-    return result
-}
+  }
+  return result;
+};
 
-console.log('First 100 Fibonacci numbers', fib(100))
+console.log("First 100 Fibonacci numbers", fib(100));
